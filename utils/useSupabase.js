d494627cdfa1a7211635bd9f2b8e3e6f1dff303b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-07 16:33:48 +0530

    Update useSupabase.js

--- utils/useSupabase.js
+++ utils/useSupabase.js
@@ -1,7 +1,19 @@
 import {createClient} from '@supabase/supabase-js'
+import {useState} from 'react'
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_API_KEY
 )
 
+const useSupabase = () => {
+    const [session, setSession] = useState(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange(
+           async (_event, session) => {
+             setSession(session)
+           },
+    )
+
+    return { session, supabase }
+}
