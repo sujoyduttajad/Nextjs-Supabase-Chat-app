e939c02d858db10c96886ead9de5426960636395
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-17 01:52:38 +0530

    Update Auth.js

--- components/Auth.js
+++ components/Auth.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Auth = () => {
-  return (
-    <div>
-        <button>Log In with Github</button>
-    </div>
-  )
+const Auth = ({ supabase }) => {
+   const signInWithGithub = async () => {
+        let { user, error } = await supabase.auth.signIn({
+            provider: 'github'
+        })
+   }
+    
+    return (
+        <div>
+            <button>Log In with Github</button>
+        </div>
+    )
 }
 
 export default Auth
