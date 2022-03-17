0320fc8d5a36cf36ec460b700a68f9236d41736a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-17 20:09:17 +0530

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Chat = ({ supabase }) => {
+  console.log(supabase);
+  
+  const getMessages = async () => {
+    let { data: messages, error } = await supabase.from("message").select("*");
+    console.log(messages)
+  };
 
-    console.log(supabase)
   return (
     <div>
-        <span>YOu 're Logged In</span>
+      <span>YOu 're Logged In</span>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
