f8d9eac0a219a929d97263574c64c37f5ca48326
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-27 01:07:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import useSupabase from '../utils/useSupabase'
 
 function MyApp({ Component, pageProps }) {
 
-  const { session, supabase } = useSupabase();
-  return <Component session={session} supabase={supabase} {...pageProps} />
+  const { currentUser, session, supabase } = useSupabase();
+  return <Component currentUser={currentUser} session={session} supabase={supabase} {...pageProps} />
 }
 
 export default MyApp
