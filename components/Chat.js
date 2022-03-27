e8355a2dd1a6ab97e2452785530bb79c1b2143c4
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-27 12:10:52 +0530

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import styles from '../styles/Chat.module.css'
+import styles from "../styles/Chat.module.css";
 
 const Chat = ({ supabase, session }) => {
   const [messages, setMessages] = useState([]);
@@ -16,41 +16,82 @@ const Chat = ({ supabase, session }) => {
 
     const setupMessagesSubscription = async () => {
       await supabase
-        .from('message')
-        .on('INSERT', payload => {
-          setMessages(prev => [].concat(prev, payload.new))
+        .from("message")
+        .on("INSERT", (payload) => {
+          setMessages((prev) => [].concat(prev, payload.new));
         })
-        .subscribe()
-    }
+        .subscribe();
+    };
     await setupMessagesSubscription();
   }, []);
-  
+
   const handleSendMessage = async (event) => {
-      event.preventDefault();
+    event.preventDefault();
 
-      const content = messageRef.current.value
-      await supabase
-      .from('message')
-      .insert([
-        { content, user_id: session.user.id }
-      ]);
+    const content = messageRef.current.value;
+    await supabase
+      .from("message")
+      .insert([{ content, user_id: session.user.id }]);
 
-      // reset the message to blank space
-      messageRef.current.value = ""
-  }
+    // reset the message to blank space
+    messageRef.current.value = "";
+  };
   return (
-    <div>
-      {messages.map((message) => (
-        <div key={message.id} className={styles.messageContainer}>
-            {message.content}
+    <>
+      {/* <div className={styles.header}>
+        <div className={styles.headerText}>
+          <h1>Supabase Chat</h1>
+          <p>
+            Welcome,{" "}
+            {currentUser.username ? currentUser.username : session.user.email}
+          </p>
         </div>
-      ))}
+        <div className={styles.settings}>
+          {editingUsername ? (
+            <form onSubmit={setUsername}>
+              <input
+                type="text"
+                required
+                ref={newUsername}
+                placeholder="New username"
+              />
+              <button type="submit">Set username</button>
+            </form>
+          ) : (
+            <>
+              <div>
+                <button onClick={() => setEditingUsername(true)}>
+                  Update username
+                </button>
+              </div>
+              <div>
+                <button onClick={signout}>Log out</button>
+              </div>
+            </>
+          )}
+        </div>
+      </div> */}
 
-      <form className={styles.chat} onSubmit={handleSendMessage}>
-        <input className={styles.messageInput} placeholder="Write your message" required ref={messageRef} />
-        <button className={styles.submit} type="submit">Send Message</button>
-      </form>
-    </div>
+      <div className={styles.container}>
+        {messages.map((message) => (
+          <div key={message.id} className={styles.messageContainer}>
+            {message.content}
+          </div>
+        ))}
+
+        <form className={styles.chat} onSubmit={handleSendMessage}>
+          <input
+            className={styles.messageInput}
+            placeholder="Write your message"
+            required
+            ref={messageRef}
+          />
+          <button className={styles.submit} type="submit">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
