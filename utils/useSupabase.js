0c95235625cb02620d07701725d14a04ed3cfa9c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-27 13:53:28 +0530

    Update useSupabase.js

--- utils/useSupabase.js
+++ utils/useSupabase.js
@@ -17,17 +17,23 @@ const useSupabase = () => {
   useEffect(async () => {
     const getCurrentUser = async () => {
       /* (session?.user.id) or (session && session.user.id ? "" : "") is an optional chaining which says if there is a session and there is .user.id, if both are false then it will just bail completely. So if there's no session it won't even try and run the code */
+      // Here the condition helps us to get the currentUser data
       if (session?.user.id) {
         const { data: currentUser } = await supabase
-          .from(`user:id=eq.${foundUser.id}`)
+          .from('user')
           .select("*")
           .eq("id", session.user.id);
-
+        /* 
+          Here the nested condition checks for null users, if not then
+          return the first element of the array into a variable 
+         */
         if (currentUser.length) {
           const foundUser = currentUser[0];
-          // To monitor any updates and persists that update into the currentUser
+         /* To monitor any updates and persists that update into the 
+          currentUser. 
+         */
           await supabase
-            .from("user")
+            .from(`user:id=eq.${foundUser.id}`)
             .on("UPDATE", (payload) => {
               setCurrentUser(payload.new);
             })
@@ -39,9 +45,9 @@ const useSupabase = () => {
       }
     };
 
-    const foundCurrentUser = await getCurrentUser();
-    setCurrentUser(foundCurrentUser)
-  }, []);
+    const foundUser = await getCurrentUser();
+    setCurrentUser(foundUser)
+  }, [session]);
 
   return { currentUser, session, supabase };
 };
