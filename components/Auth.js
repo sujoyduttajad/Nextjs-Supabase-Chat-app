c4b7c983b46e05d6c93087e89f126b0346f57662
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-03-27 18:02:31 +0530

    Update Auth.js

--- components/Auth.js
+++ components/Auth.js
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import styles from '../styles/Auth.module.css'
 
 const Auth = ({ supabase }) => {
-   const signInWithGithub = () => {
-        supabase.auth.signIn({
-            provider: 'github'
-        })
-   }
-    
-    return (
-        <div>
-            <button onClick={signInWithGithub}>Log In with Github</button>
-        </div>
-    )
-}
+  const [error, setError] = useState("");
+  const [sentEmail, setSentEmail] = useState(false);
 
-export default Auth
+  const emailRef = useRef(null);
+  const signIn = async (evt) => {
+    evt.preventDefault();
+    const email = emailRef.current.value;
+    const { error } = await supabase.auth.signIn({ email });
+    error ? setError(error.message) : setSentEmail(true);
+  };
+
+  const signInWithGithub = (evt) => {
+    evt.preventDefault();
+    supabase.auth.signIn({
+      provider: "github",
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Supabase Chat</h1>
+
+      {error ? <p className={styles.error}>{error}</p> : null}
+
+      {sentEmail ? (
+        <p>We've sent you an email to login! Check your email to continue.</p>
+      ) : (
+        <>
+          <form onSubmit={signIn}>
+            <input
+              className={styles.input}
+              placeholder="your@email.com"
+              type="text"
+              ref={emailRef}
+              required
+            />
+
+            <button className={styles.submit} type="submit">
+              Login
+            </button>
+          </form>
+
+          <p>
+            <button className={styles.github} onClick={signInWithGithub}>
+              Sign in with GitHub
+            </button>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
