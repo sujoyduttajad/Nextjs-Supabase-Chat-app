20f5700c9f8aaa74a355bbbbe2b940ddeed0e4a9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-12 17:36:13 +0530

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -143,7 +143,7 @@ const Chat = ({ currentUser, supabase, session }) => {
       <nav className={styles.header}>
         <div className={styles.headerText}>
           <div className={styles.brandName}>
-            <Image width={50} height={23} src={Brand} alt="Brand icon" />
+            <Image width={100} height={100} src={Brand} alt="Brand icon" />
             <h1 className={styles.h1}>Enigma</h1>
           </div>
           
