dbb8bf5d2223bb4b1bea321ad5a20827df6f2bfe
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-13 11:26:32 +0530

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "../styles/Chat.module.css";
-import Image from 'next/image'
-import logo from '../fonts/send-logo.svg'
-import Brand from '../fonts/invert-brand.svg'
+import Image from "next/image";
+import logo from "../fonts/send-logo.svg";
+import Brand from "../fonts/invert-brand.svg";
 
 const Chat = ({ currentUser, supabase, session }) => {
-  if(!currentUser) return null;
-  // Loading screen will be here 
+  if (!currentUser) return null;
+  // Loading screen will be here
 
   const [messages, setMessages] = useState([]);
   const [editingUsername, setEditingUsername] = useState(false);
   const [users, setUsers] = useState({});
 
   const messageRef = useRef("");
-  const newUsername = useRef("")
+  const newUsername = useRef("");
 
   useEffect(async () => {
     const getMessages = async () => {
@@ -34,59 +34,57 @@ const Chat = ({ currentUser, supabase, session }) => {
         .subscribe();
     };
     await setupMessagesSubscription();
-    
+
     // Subscription to changes on UPDATE
     const setupUsersSubscription = async () => {
       await supabase
-        .from('user')
-        .on('UPDATE', payload => {
-          setUsers(users => {
+        .from("user")
+        .on("UPDATE", (payload) => {
+          setUsers((users) => {
             const user = users[payload.new.id];
-            if(user) {
+            if (user) {
               return Object.assign({}, users, {
-                [payload.new.id]: payload.new
-              })
+                [payload.new.id]: payload.new,
+              });
             } else {
               return users;
             }
-          })
+          });
         })
-        .subscribe()
-    }
+        .subscribe();
+    };
     await setupUsersSubscription();
   }, []);
 
-  // Using supabase API look at the users that we already have. It will consists of objects with a bunch of users 
+  // Using supabase API look at the users that we already have. It will consists of objects with a bunch of users
   const getUsersFromSupabase = async (users, userIds) => {
-    const usersToGet = Array.from(userIds).filter(userId => !users[userId])
-    
+    const usersToGet = Array.from(userIds).filter((userId) => !users[userId]);
+
     /* ------ Case 1 ------- */
     if (Object.keys(users).length && usersToGet.length === 0) {
       return users;
     }
     /* ------ Case 2 ------- */
     const { data } = await supabase
-      .from('user')
-      .select('id,username')
-      .in('id', usersToGet);
+      .from("user")
+      .select("id,username")
+      .in("id", usersToGet);
 
     const newUsers = {};
-    data.forEach(user => newUsers[user.id] = user)
+    data.forEach((user) => (newUsers[user.id] = user));
 
-    return Object.assign({}, users, newUsers)
-  }
+    return Object.assign({}, users, newUsers);
+  };
 
-   
-
-  // Request User Details for a Given User and get their userName & userId. 
+  // Request User Details for a Given User and get their userName & userId.
   useEffect(async () => {
     const getUsers = async () => {
-      const userIds = new Set(messages.map(message => message.user_id));
-      const newUsers = await getUsersFromSupabase(users, userIds)
+      const userIds = new Set(messages.map((message) => message.user_id));
+      const newUsers = await getUsersFromSupabase(users, userIds);
       setUsers(newUsers);
-    }
+    };
     await getUsers();
-  }, [messages])
+  }, [messages]);
 
   const handleSendMessage = async (event) => {
     event.preventDefault();
@@ -100,95 +98,105 @@ const Chat = ({ currentUser, supabase, session }) => {
     messageRef.current.value = "";
   };
 
-  const signout = event => {
+  const signout = (event) => {
     event.preventDefault();
     window.localStorage.clear();
     window.location.reload();
-  }
+  };
 
-  const setUsername = async evt => {
+  const setUsername = async (evt) => {
     evt.preventDefault();
     try {
       const username = newUsername.current.value;
       await supabase
-      .from('user')
-      .insert([
-        { ...currentUser, username }
-      ], { upsert: true }); // 'upsert' is a combination of insert and update
-  
-      newUsername.current.value = ""
-      setEditingUsername(false)
+        .from("user")
+        .insert([{ ...currentUser, username }], { upsert: true }); // 'upsert' is a combination of insert and update
+
+      newUsername.current.value = "";
+      setEditingUsername(false);
     } catch (err) {
       console.log("Something went wrong");
     }
-  }
+  };
 
   const username = (user_id) => {
     const user = users[user_id];
     // Check for user exists or not. A loading component could be created here
-    if (!user) 
-      return ""
-    return user.username ? user.username : user.id
-  }
+    if (!user) return "";
+    return user.username ? user.username : user.id;
+  };
   const timestamp = (created_at) => {
     const user = users[created_at];
     // Check for user exists or not. A loading component could be created here
-    if (!user) 
-      return ""
-    return user.username ? user.created_at : user.id
-  }
+    if (!user) return "";
+    return user.username ? user.created_at : user.id;
+  };
 
   return (
     <>
       <nav className={styles.header}>
         <div className={styles.headerText}>
           <div className={styles.brandName}>
-            <Image className={styles.imageCon} src={Brand} alt="Brand icon" shape-rendering="geometricPrecision" />
+            <Image
+              className={styles.imageCon}
+              src={Brand}
+              alt="Brand icon"
+              shape-rendering="geometricPrecision"
+            />
             <h1 className={styles.h1}>Enigma</h1>
           </div>
-          
+
           <p className={styles.p}>
-            Welcome, {currentUser.username ? currentUser.username : session.user.email}
+            Welcome,{" "}
+            {currentUser.username ? currentUser.username : session.user.email}
           </p>
         </div>
-        
+
         <div className={styles.settings}>
-          {editingUsername ?
+          {editingUsername ? (
             <form className={styles.usernameForm} onSubmit={setUsername}>
-              <input className={styles.messageInput} type="text" required ref={newUsername} placeholder="New username" />
-              <button className={styles.submit} type="submit">Set username</button>
+              <input
+                className={styles.messageInput}
+                type="text"
+                required
+                ref={newUsername}
+                placeholder="New username"
+              />
+              <button className={styles.submit} type="submit">
+                Set username
+              </button>
             </form>
-            : (
-              <>
-                <div className={styles.buttonContainer}>
-                  <button 
-                    style={{ 
-                      fontSize: '0.95em', 
-                      fontWeight: 200,
-                      letterSpacing: '1.2px',
-                      marginBottom: '0.5em'
-                    }}
-                    className={styles.submit} 
-                    onClick={() => setEditingUsername(true)}
-                  >
-                    <p>Update Username</p>
-                  </button>
-                </div>
-                <div className={styles.buttonContainer}>
-                  <button 
-                    style={{ 
-                      fontSize: '0.95em',
-                      fontWeight: 200,
-                      letterSpacing: '1.2px',
-                    }}
-                    className={styles.submit} 
-                    onClick={signout}
-                  >
-                    <p>Log out</p>
-                  </button>
-                </div>
-              </>
-            )}
+          ) : (
+            <>
+              <div className={styles.buttonContainer}>
+                <button
+                  style={{
+                    fontSize: "0.95em",
+                    fontWeight: 200,
+                    letterSpacing: "1.2px",
+                    marginBottom: "0.5em",
+                  }}
+                  className={styles.submit}
+                  onClick={() => setEditingUsername(true)}
+                >
+                  <p>Update Username</p>
+                </button>
+              </div>
+              <div className={styles.buttonContainer}>
+                <button
+                  style={{
+                    fontSize: "0.95em",
+                    fontWeight: 200,
+                    letterSpacing: "1.2px",
+                  }}
+                  className={styles.submit}
+                  onClick={signout}
+                >
+                  <p>Log out</p>
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </nav>
 
