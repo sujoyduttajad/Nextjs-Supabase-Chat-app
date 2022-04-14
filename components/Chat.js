7f8242ffa1b46021aaf9ad6d0b5c54266209f850
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-14 17:30:53 +0530

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from "react";
 import styles from "../styles/Chat.module.scss";
 import Image from "next/image";
 import logo from "../fonts/send-logo.svg";
-import Brand from "../fonts/invert-brand.svg";
 import { format, toDate } from 'date-fns'
 
 const Chat = ({ currentUser, supabase, session }) => {
@@ -137,71 +136,7 @@ const Chat = ({ currentUser, supabase, session }) => {
 
   return (
     <>
-      <nav className={styles.header}>
-        <div className={styles.headerText}>
-          <div className={styles.brandName}>
-            <Image
-              className={styles.imageCon}
-              src={Brand}
-              alt="Brand icon"
-              shape-rendering="geometricPrecision"
-            />
-            <h1 className={styles.h1}>Enigma</h1>
-          </div>
-          <hr className={styles.hr}/>
-          <p className={styles.p}>
-            Welcome,{" "}
-            {currentUser.username ? currentUser.username : session.user.email}
-          </p>
-        </div>
-        
-        <div className={styles.settings}>
-          {editingUsername ? (
-            <form className={styles.usernameForm} onSubmit={setUsername}>
-              <input
-                className={styles.messageInput}
-                type="text"
-                required
-                ref={newUsername}
-                placeholder="New username"
-              />
-              <button className={styles.submit} type="submit">
-                Set username
-              </button>
-            </form>
-          ) : (
-            <>
-              <div className={styles.buttonContainer}>
-                <button
-                  style={{
-                    fontSize: "0.95em",
-                    fontWeight: 600,
-                    letterSpacing: "1.2px",
-                    marginBottom: "0.5em",
-                  }}
-                  className={styles.submit}
-                  onClick={() => setEditingUsername(true)}
-                >
-                  <p>Update Username</p>
-                </button>
-              </div>
-              <div className={styles.buttonContainer}>
-                <button
-                  style={{
-                    fontSize: "0.95em",
-                    fontWeight: 600,
-                    letterSpacing: "1.2px",
-                  }}
-                  className={styles.submit}
-                  onClick={signout}
-                >
-                  <p>Log out</p>
-                </button>
-              </div>
-            </>
-          )}
-        </div>
-      </nav>
+      
 
       <div className={styles.container}>
         {messages.sort(function(a,b) {
