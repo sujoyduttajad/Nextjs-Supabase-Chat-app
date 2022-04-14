83535f5b607afe515fb3e31fe5e8134724e09803
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-14 18:54:28 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,41 @@
-import '../styles/globals.scss'
-import useSupabase from '../utils/useSupabase'
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import "../styles/globals.scss";
+import useSupabase from "../utils/useSupabase";
 
 function MyApp({ Component, pageProps }) {
-
   const { currentUser, session, supabase } = useSupabase();
-  return <Component currentUser={currentUser} session={session} supabase={supabase} {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <title>Watdo</title>
+      </Head>
+      <Component
+        currentUser={currentUser}
+        session={session}
+        supabase={supabase}
+        {...pageProps}
+      />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
