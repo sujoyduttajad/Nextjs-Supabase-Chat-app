d7cba4176a81068e6fb3a27b6de87bbdd7e62b88
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-14 18:55:08 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,7 +2,6 @@ import React from "react";
 import Brand from "../fonts/invert-brand.svg";
 import styles from "../styles/Chat.module.scss";
 import Image from "next/image";
-import { CAvatar } from '@coreui/react'
 
 const Sidebar = ({
   currentUser,
@@ -26,14 +25,14 @@ const Sidebar = ({
         </div>
         <hr className={styles.hr} />
         <div className={styles.welcome}>
-            <CAvatar
+            {/* <CAvatar
             shape="rounded-10"
                 src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" 
                 className={styles.cavatar} 
                 status="success"
             >
                 {currentUser.username ? currentUser.username.slice(0,1) : session.user.email.slice(0,1)}
-            </CAvatar>  
+            </CAvatar>   */}
           <p className={styles.p}>
             Welcome,{" "}
             <span>{currentUser.username ? currentUser.username : session.user.email}</span>
