928559c9213b472d6712b361795627a0f3645495
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-16 23:57:49 +0530

    Create data.js

---
+++ utils/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: 1,
+        imageUrl: "https://i.ibb.co/wRGrnt2/Vector-images-1.png",
+    },
+    {
+        id: 2,
+        imageUrl: "https://i.ibb.co/CQmnKJ0/Vector-images-2.png"
+    },
+    {
+        id: 3,
+        imageUrl: "https://i.ibb.co/LxxRCdP/Vector-images-3.png" 
+    },
+    {
+        id: 4,
+        imageUrl: "https://i.ibb.co/zXxmmq9/Vector-images-4.png"
+    },
+    {
+        id: 5,
+        imageUrl: "https://i.ibb.co/4tL1JVx/Vector-images-5.png" 
+    },
+    {
+        id: 6,
+        imageUrl: "https://i.ibb.co/yYv0t3B/Vector-images-6.png" 
+    },
+    {
+        id: 7,
+        imageUrl: "https://i.ibb.co/7ztNZxj/Vector-images-7.png" 
+    },
+    {
+        id: 8,
+        imageUrl: "https://i.ibb.co/hHTzg3T/Vector-images-8.png" 
+    },
+];
