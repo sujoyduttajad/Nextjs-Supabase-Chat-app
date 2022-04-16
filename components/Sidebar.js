8075fc75d85b2add5fd25b5825836d1fe8d49d7a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-16 21:11:44 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -26,21 +26,21 @@ const StyledBadge = withStyles((theme) => ({
       width: "100%",
       height: "100%",
       borderRadius: "50%",
-      animation: "$ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
+      // animation: "$ripple 1.2s infinite ease-in-out",
+      // border: "1px solid currentColor",
       content: '""',
     },
   },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
-    },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
+  // "@keyframes ripple": {
+  //   "0%": {
+  //     transform: "scale(.8)",
+  //     opacity: 1,
+  //   },
+  //   "100%": {
+  //     transform: "scale(2.4)",
+  //     opacity: 0,
+  //   },
+  // },
 }))(Badge);
 
 const useStyles = makeStyles((theme) => ({
