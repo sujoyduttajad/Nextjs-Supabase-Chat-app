b36686e1792883caf69e6384600d1a5aff472996
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-17 13:55:01 +0530

    Update Loader.js

--- components/Loader.js
+++ components/Loader.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Brand from "../images/invert-brand.svg";
+import styles from "../styles/Loader.module.scss";
+import Image from "next/image";
 
 const Loader = () => {
   return (
