b921e1d43f30b59fd4c606e1fcc06698f59f9366
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-17 15:43:01 +0530

    Update Loader.js

--- components/Loader.js
+++ components/Loader.js
@@ -6,23 +6,26 @@ import Image from "next/image";
 const Loader = () => {
   return (
     <div className={styles.body}>
-      <div class="loader">
+      {/* <div className={styles.loader}>
         <svg viewBox="0 0 80 80">
           <circle id="test" cx="40" cy="40" r="32"></circle>
         </svg>
+      </div> */}
+
+      <div className={styles.loader}>
+        <Image
+            className={styles.imageCon}
+            src={Brand}
+            alt="Brand icon"
+            shape-rendering="geometricPrecision"
+          />
       </div>
 
-      <div class="loader triangle">
-        <svg viewBox="0 0 86 80">
-          <polygon points="43 8 79 72 7 72"></polygon>
-        </svg>
-      </div>
-
-      <div class="loader">
+      {/* <div className={styles.loader}>
         <svg viewBox="0 0 80 80">
           <rect x="8" y="8" width="64" height="64"></rect>
         </svg>
-      </div>
+      </div> */}
 
       <a
         class="dribbble"
