6dee17798e8d924f2ab218c806e6035814dcd921
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-17 13:59:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ currentUser, session, supabase }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Supabase Chat App</title>
+        <title>Enigma</title>
       </Head>
 
       <main className={styles.main}>
