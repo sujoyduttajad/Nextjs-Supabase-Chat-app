d64dd7429aab6a8f51e23f1f0aaad7d6055a79d3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-17 13:59:05 +0530

    Update profile.js

---
+++ pages/settings/profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from '../../components/Loader'
+
+const Profile = () => {
+  return (
+    <div>
+        <Loader/>
+    </div>
+  )
+}
+
+export default Profile
