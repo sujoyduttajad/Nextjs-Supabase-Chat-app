ea63b1e14f3e45d5affd57abe2bc538396c21bff
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-20 13:21:33 +0530

    Update profile.js

--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Loader from '../../components/Loader'
+import Sidebar from '../../components/Sidebar'
 
 const Profile = () => {
   return (
     <div>
-        <Loader/>
+        <Sidebar/>
     </div>
   )
 }
