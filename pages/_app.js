c9cda266e53102d497464830adb402a2627952b1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-22 16:04:41 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Head from "next/head";
 import "../styles/globals.scss";
 import useSupabase from "../utils/useSupabase";
 
 function MyApp({ Component, pageProps }) {
   const { currentUser, session, supabase } = useSupabase();
+
+  // Remove the server-side injected CSS.
   React.useEffect(() => {
-    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
@@ -16,13 +16,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <React.Fragment>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <title>Watdo</title>
-      </Head>
       <Component
         currentUser={currentUser}
         session={session}
