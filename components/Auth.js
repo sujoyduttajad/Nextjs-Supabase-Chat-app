0234b27a692d2ee1476acb39197ca992b617dbd3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-24 12:17:49 +0530

    Update Auth.js

--- components/Auth.js
+++ components/Auth.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from "react"
-import styles from "../styles/Auth.module.scss"
-import Brand from "../images/invert-brand.svg"
-import Image from "next/image"
+import React, { useRef, useState } from "react";
+import styles from "../styles/Auth.module.scss";
+import Brand from "../images/invert-brand.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 const Auth = ({ supabase }) => {
   const [error, setError] = useState("");
@@ -22,37 +23,58 @@ const Auth = ({ supabase }) => {
     });
   };
 
+  // const { user, session, error } = await supabase.auth.signUp({
+  //   email: 'example@email.com',
+  //   password: 'example-password',
+  // })
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-      <div className={styles.brandName}>
-            <Image className={styles.imageCon} src={Brand} alt="Brand icon" shapeRendering="geometricPrecision" />
-            <h1 className={styles.title}>Enigma</h1>
-          </div>
-      {error ? <p className={styles.error}>{error}</p> : null}
-
-      {sentEmail ? (
-        <p>We have sent you an email to login! Check your email to continue.</p>
-      ) : (
-        <div className={styles.formContainer}>
-          <form onSubmit={signIn}>
-            <input
-              className={styles.input}
-              placeholder="your@email.com"
-              type="text"
-              ref={emailRef}
-              required
-            />
-            <button className={styles.submit} type="submit">
-              Login
-            </button>
-          </form>
-          <hr className={styles.hr}/>
-          <button className={styles.github} onClick={signInWithGithub}>
-            <p>Sign in with GitHub </p>
-          </button>  
+        <div className={styles.brandName}>
+          <Image
+            className={styles.imageCon}
+            src={Brand}
+            alt="Brand icon"
+            shapeRendering="geometricPrecision"
+          />
+          <h1 className={styles.title}>Enigma</h1>
         </div>
-      )}
+        {error ? <p className={styles.error}>{error}</p> : null}
+
+        {sentEmail ? (
+          <p>
+            We have sent you an email to login! Check your email to continue.
+          </p>
+        ) : (
+          <div className={styles.formContainer}>
+            <form onSubmit={signIn}>
+              <input
+                className={styles.input}
+                placeholder="your@email.com"
+                type="text"
+                ref={emailRef}
+                required
+              />
+              <button className={styles.submit} type="submit">
+                Login
+              </button>
+            </form>
+            <hr className={styles.hr} />
+            <button className={styles.github} onClick={signInWithGithub}>
+              <p>Sign in with GitHub </p>
+            </button>
+            <div>
+              <p>
+                Don't have an account?
+                <Link href="/register" passHref>
+                  <span> Click here </span>
+                </Link>
+                to create one
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
