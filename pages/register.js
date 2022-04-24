f96055d0741ce99a0a4ac9f338d98b342fa628c1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-24 12:48:54 +0530

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import styles from "../styles/Auth.module.scss";
 
 const Register = () => {
-  return (
-    <div>register</div>
-  )
-}
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
-export default Register
+  const signUp = async (evt) => {
+    evt.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const { user, session, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={signUp}>
+        <input
+          className={styles.input}
+          placeholder="your@email.com"
+          type="text"
+          ref={emailRef}
+          required
+        />
+        <input
+          className={styles.input}
+          placeholder="password"
+          type="password"
+          ref={passwordRef}
+          required
+        />
+        <button className={styles.submit} type="submit">
+            Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
